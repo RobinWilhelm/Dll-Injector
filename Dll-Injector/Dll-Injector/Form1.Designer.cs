c86504b829f15437cfa20e654c999e2bd31cb134
Robin <the.one.and.only.inno@gmail.com>
2019-01-29 21:01:53 +0100

    Initial Commit

---
+++ Dll-Injector/Dll-Injector/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Dll_Injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btRefreshProcesses = new System.Windows.Forms.Button();
+            this.clbProcesslist = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btRefreshDlls = new System.Windows.Forms.Button();
+            this.btInject = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSelectedDll = new System.Windows.Forms.TextBox();
+            this.cbOnlyWindowed = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btRefreshProcesses
+            // 
+            this.btRefreshProcesses.Location = new System.Drawing.Point(12, 385);
+            this.btRefreshProcesses.Name = "btRefreshProcesses";
+            this.btRefreshProcesses.Size = new System.Drawing.Size(340, 23);
+            this.btRefreshProcesses.TabIndex = 1;
+            this.btRefreshProcesses.Text = "Refresh";
+            this.btRefreshProcesses.UseVisualStyleBackColor = true;
+            this.btRefreshProcesses.Click += new System.EventHandler(this.btRefreshProcesses_Click);
+            // 
+            // clbProcesslist
+            // 
+            this.clbProcesslist.FormattingEnabled = true;
+            this.clbProcesslist.Location = new System.Drawing.Point(12, 33);
+            this.clbProcesslist.Name = "clbProcesslist";
+            this.clbProcesslist.Size = new System.Drawing.Size(340, 319);
+            this.clbProcesslist.TabIndex = 2;
+            this.clbProcesslist.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.clbProcesslist_ItemCheck);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Processes";
+            // 
+            // btRefreshDlls
+            // 
+            this.btRefreshDlls.Location = new System.Drawing.Point(7, 456);
+            this.btRefreshDlls.Name = "btRefreshDlls";
+            this.btRefreshDlls.Size = new System.Drawing.Size(344, 23);
+            this.btRefreshDlls.TabIndex = 5;
+            this.btRefreshDlls.Text = "Select Dll";
+            this.btRefreshDlls.UseVisualStyleBackColor = true;
+            this.btRefreshDlls.Click += new System.EventHandler(this.btSelectDll_Click);
+            // 
+            // btInject
+            // 
+            this.btInject.Location = new System.Drawing.Point(7, 520);
+            this.btInject.Name = "btInject";
+            this.btInject.Size = new System.Drawing.Size(344, 23);
+            this.btInject.TabIndex = 6;
+            this.btInject.Text = "Inject";
+            this.btInject.UseVisualStyleBackColor = true;
+            this.btInject.Click += new System.EventHandler(this.btInject_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 432);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dll";
+            // 
+            // tbSelectedDll
+            // 
+            this.tbSelectedDll.Location = new System.Drawing.Point(7, 485);
+            this.tbSelectedDll.Name = "tbSelectedDll";
+            this.tbSelectedDll.ReadOnly = true;
+            this.tbSelectedDll.Size = new System.Drawing.Size(344, 20);
+            this.tbSelectedDll.TabIndex = 9;
+            // 
+            // cbOnlyWindowed
+            // 
+            this.cbOnlyWindowed.AutoSize = true;
+            this.cbOnlyWindowed.Location = new System.Drawing.Point(12, 358);
+            this.cbOnlyWindowed.Name = "cbOnlyWindowed";
+            this.cbOnlyWindowed.Size = new System.Drawing.Size(160, 17);
+            this.cbOnlyWindowed.TabIndex = 10;
+            this.cbOnlyWindowed.Text = "only processes with window ";
+            this.cbOnlyWindowed.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 555);
+            this.Controls.Add(this.cbOnlyWindowed);
+            this.Controls.Add(this.tbSelectedDll);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btInject);
+            this.Controls.Add(this.btRefreshDlls);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clbProcesslist);
+            this.Controls.Add(this.btRefreshProcesses);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Dll Injector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btRefreshProcesses;
+        private System.Windows.Forms.CheckedListBox clbProcesslist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btRefreshDlls;
+        private System.Windows.Forms.Button btInject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSelectedDll;
+        private System.Windows.Forms.CheckBox cbOnlyWindowed;
+    }
+}
+
---
+++ Dll-Injector/Dll-Injector/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Dll_Injector
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processList = new List<Process>();
+        private string selectedDll;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Text += (IsX86Injector()) ? " 32Bit Mode" : " 64Bit Mode";
+
+            RefreshProcesslist();
+        }
+
+        private void btRefreshProcesses_Click(object sender, EventArgs e)
+        {
+            RefreshProcesslist();
+        }
+
+        private void btSelectDll_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "DLL files | *.dll";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedDll = openFileDialog.FileName;
+                tbSelectedDll.Text = openFileDialog.SafeFileName;
+            } 
+        }
+
+        private void clbProcesslist_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+                for (int ix = 0; ix < clbProcesslist.Items.Count; ++ix)
+                    if (e.Index != ix) clbProcesslist.SetItemChecked(ix, false);
+        }   
+
+        private bool IsX86Injector()
+        {
+            return (IntPtr.Size == 4);
+        }
+
+        private void RefreshProcesslist()
+        {
+            clbProcesslist.DataSource = null;
+            processList.Clear();
+
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                IntPtr handle = Kernel32.OpenProcess((uint)Kernel32.ProcessAccessType.PROCESS_QUERY_LIMITED_INFORMATION, false, (uint)p.Id);
+                bool x86Process;
+                Kernel32.IsWow64Process(handle, out x86Process);
+                Kernel32.CloseHandle(handle);
+
+                if (!(IsX86Injector() == x86Process))
+                {
+                    continue;
+                }
+
+                if(cbOnlyWindowed.Checked)
+                {
+                    if(p.MainWindowHandle == (IntPtr)0)
+                    {
+                        continue;
+                    }
+                }
+
+                // too many of these
+                if (p.ProcessName == "svchost")
+                    continue;
+
+                processList.Add(p);
+            }
+
+            ((ListBox)clbProcesslist).DataSource = processList;
+            ((ListBox)clbProcesslist).DisplayMember = "ProcessName";
+            ((ListBox)clbProcesslist).ValueMember = "Id";  
+        }        
+
+        
+
+        private void btInject_Click(object sender, EventArgs e)
+        {
+            if (selectedDll == null) return;
+
+            foreach(Process target in clbProcesslist.CheckedItems)
+            {            
+                // 1 Verbindung zu Zielprozess herstellen (Handle)
+                uint access = (uint)(Kernel32.ProcessAccessType.PROCESS_CREATE_THREAD | 
+                    Kernel32.ProcessAccessType.PROCESS_VM_WRITE | Kernel32.ProcessAccessType.PROCESS_VM_OPERATION);
+                IntPtr handle = Kernel32.OpenProcess(access, false, (uint)target.Id);
+                if (handle == null) continue;
+
+                // 2 Speicheradresse der Funktion LoadLibrary bestimmen 
+                IntPtr LoadLibraryFn = Kernel32.GetProcAddress(Kernel32.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+                if (LoadLibraryFn == null)
+                {
+                    Kernel32.CloseHandle(handle);
+                    continue;
+                }                
+
+                // 3 Speicher im Zielprozess reservieren
+                IntPtr address = Kernel32.VirtualAllocEx(handle, (IntPtr)null, Convert.ToUInt32(selectedDll.Length), 
+                    Kernel32.AllocationType.Reserve | Kernel32.AllocationType.Commit, Kernel32.MemoryProtection.ReadWrite);
+                if (address == null)
+                {
+                    Kernel32.CloseHandle(handle);
+                    continue;
+                }
+
+                // 4 DLL Pfad in den reservierten Speicher schreiben
+                byte[] buffer = Encoding.ASCII.GetBytes(selectedDll);
+                bool success = Kernel32.WriteProcessMemory(handle, address, buffer, (uint)buffer.Length, 0);
+                if (!success)
+                {
+                    Kernel32.CloseHandle(handle);
+                    continue;
+                }
+
+                // 5 Thread im Zielprozess erstellen und dort LoadLibrary mit der Adresse als Parameter ausführen
+                IntPtr tmp;
+                IntPtr thread = Kernel32.CreateRemoteThread(handle, (IntPtr)null, 0, LoadLibraryFn, address, 0, out tmp);
+                if (thread == null)
+                {
+                    Kernel32.CloseHandle(handle);
+                    continue;
+                }
+
+                // 6 Verbindungen schließen
+                Kernel32.CloseHandle(thread);
+                Kernel32.CloseHandle(handle);
+            }           
+        }       
+    }
+}
