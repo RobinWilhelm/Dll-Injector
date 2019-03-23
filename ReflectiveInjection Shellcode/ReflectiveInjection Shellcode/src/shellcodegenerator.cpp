8576f9864396708114445a6be49479532a6c6fe4
Robin <mail@robinwilhelm.de>
2019-03-23 16:41:34 +0100

    finished and tested shellcode in test environment

--- ReflectiveInjection Shellcode/ReflectiveInjection Shellcode/src/shellcodegenerator.cpp
+++ ReflectiveInjection Shellcode/ReflectiveInjection Shellcode/src/shellcodegenerator.cpp
@@ -1,54 +1,65 @@
 #include <windows.h>
 #include <stdio.h>
-#include <winnt.h>
 
-typedef HMODULE(*LoadLibraryFunction) (LPCSTR lpLibFileName);
-typedef LPVOID(*VirtualAllocFunction) (LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
-typedef FARPROC(*GetProcAddressFunction) (HMODULE hModule, LPCSTR lpProcName);
+//#define IMAGE_ORDINAL(ordinal) (ordinal & 0xFFFF)
 
-typedef struct _ShellcodeInformation 
+typedef BOOL (WINAPI *DllMainFunction)(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved);
+typedef HMODULE(WINAPI *LoadLibraryFunction) (LPCSTR lpLibFileName);
+typedef LPVOID  (WINAPI *VirtualAllocFunction) (LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
+typedef FARPROC(WINAPI *GetProcAddressFunction) (HMODULE hModule, LPCSTR lpProcName);
+typedef BOOL(WINAPI *VirtualFreeFunction)(LPVOID lpAddress,	SIZE_T dwSize, DWORD  dwFreeType);
+typedef BOOL(WINAPI *VirtualProtectFunction)(LPVOID lpAddress, SIZE_T dwSize, DWORD  flNewProtect, PDWORD lpflOldProtect);
+
+
+
+typedef struct 
 {
 	byte* raw_module_destination;
-	LoadLibraryFunction fnLoadLibrary;
-	VirtualAllocFunction fnVirtualAlloc;
+	DWORD size_of_raw_module;
+	// i dont want the shellcode to search for the functions if the injector already has functionality to do this
+	LoadLibraryFunction fnLoadLibrary;	 	
 	GetProcAddressFunction fnGetProcAddress;
+	VirtualAllocFunction fnVirtualAlloc;
+	VirtualFreeFunction fnVirtualFree;
+	VirtualProtectFunction fnVirtualProtect;
 } ShellcodeInformation;
 
-void LoadLibShellcode(ShellcodeInformation* modinfo)
+PVOID LoadLibShellcode(ShellcodeInformation* modinfo)
 {		   
 	IMAGE_DOS_HEADER* idh = (IMAGE_DOS_HEADER*)modinfo->raw_module_destination;
 
 	if (idh->e_magic != IMAGE_DOS_SIGNATURE) {
-		return;
+		return  0;
 	}			
 					
-	IMAGE_NT_HEADERS* headers = ((IMAGE_NT_HEADERS*)modinfo->raw_module_destination) + idh->e_lfanew;
+	IMAGE_NT_HEADERS* headers = (IMAGE_NT_HEADERS*)(modinfo->raw_module_destination + idh->e_lfanew);
 	   
 	if (headers->Signature != IMAGE_NT_SIGNATURE) {
-		return;
+		return 0;
 	}	
 					
 	// reserve memory for the complete module
-	byte* module_destination = (byte*)modinfo->fnVirtualAlloc(modinfo->raw_module_destination, headers->OptionalHeader.SizeOfImage, MEM_RESERVE, PAGE_READWRITE);
-
+	byte* module_destination = (byte*)modinfo->fnVirtualAlloc((LPVOID)headers->OptionalHeader.ImageBase, headers->OptionalHeader.SizeOfImage, MEM_RESERVE, PAGE_READWRITE);
+	
 
 	//
 	// Map headers into memory
 	//	  
-	modinfo->fnVirtualAlloc(modinfo->raw_module_destination, headers->OptionalHeader.SizeOfHeaders, MEM_COMMIT, PAGE_READWRITE);
+	modinfo->fnVirtualAlloc(module_destination, headers->OptionalHeader.SizeOfHeaders, MEM_COMMIT, PAGE_READWRITE);
 	for (SIZE_T index = 0; index < headers->OptionalHeader.SizeOfHeaders; index++)
 	{
 		module_destination[index] = modinfo->raw_module_destination[index];
 	}	   
 	// Update pointer to headers
-	IMAGE_NT_HEADERS* new_headers = (IMAGE_NT_HEADERS*)module_destination + idh->e_lfanew;
+	IMAGE_NT_HEADERS* new_headers = (IMAGE_NT_HEADERS*)(module_destination + idh->e_lfanew);
+
 
 	//
 	// Map sections into memory
 	//						
 	for (int section_counter = 0; section_counter < new_headers->FileHeader.NumberOfSections; section_counter++)
 	{
-		IMAGE_SECTION_HEADER* ish = (IMAGE_SECTION_HEADER *)((DWORD)&(new_headers->OptionalHeader) + (section_counter * sizeof(IMAGE_OPTIONAL_HEADER)));
+		IMAGE_SECTION_HEADER* ish = (IMAGE_SECTION_HEADER *)((DWORD)&new_headers->OptionalHeader + new_headers->FileHeader.SizeOfOptionalHeader + section_counter * sizeof(IMAGE_SECTION_HEADER));
 		byte* section_destination = 0;
 		if (ish->SizeOfRawData != 0)
 		{
@@ -72,7 +83,7 @@ void LoadLibShellcode(ShellcodeInformation* modinfo)
 				{
 					(module_destination + ish->VirtualAddress)[index] = 0;
 				}
-				
+				ish->SizeOfRawData = new_headers->OptionalHeader.SizeOfInitializedData;
 			}
 			else if (ish->Characteristics == IMAGE_SCN_CNT_UNINITIALIZED_DATA)
 			{
@@ -81,7 +92,8 @@ void LoadLibShellcode(ShellcodeInformation* modinfo)
 				for (SIZE_T index = 0; index < new_headers->OptionalHeader.SizeOfUninitializedData; index++)
 				{
 					(module_destination + ish->VirtualAddress)[index] = 0;
-				}	 					
+				}	
+				ish->SizeOfRawData = new_headers->OptionalHeader.SizeOfUninitializedData;
 			}	 			
 		}
 
@@ -92,16 +104,14 @@ void LoadLibShellcode(ShellcodeInformation* modinfo)
 
 	//
 	// Perform Relocations
-	//
-
-	DWORD delta = (DWORD)module_destination - headers->OptionalHeader.ImageBase;
+	//	 
+	DWORD delta = (DWORD)(module_destination - new_headers->OptionalHeader.ImageBase);
 	IMAGE_DATA_DIRECTORY* relocation_directory_entry = &new_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
 	IMAGE_BASE_RELOCATION* relocation_table = (IMAGE_BASE_RELOCATION*)(module_destination + relocation_directory_entry->VirtualAddress);
 
-	int index = 0;
 	while (relocation_table->VirtualAddress > 0)
 	{
-		for (int relocations = 0; relocations < ((relocation_table[index].SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / 2); relocations++)
+		for (int relocations = 0; relocations < ((relocation_table->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / 2); relocations++)
 		{
 			WORD reloc_info = *(WORD*)(relocation_table + sizeof(IMAGE_BASE_RELOCATION) + (relocations * sizeof(WORD)));
 			int type, offset;
@@ -117,28 +127,171 @@ void LoadLibShellcode(ShellcodeInformation* modinfo)
 		relocation_table += relocation_table->SizeOfBlock;
 	}
 
+
 	//
-	// Imports
+	// Resolve Imports
 	//
+	IMAGE_DATA_DIRECTORY* import_directory_entry = &new_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+	IMAGE_IMPORT_DESCRIPTOR* import_table = (IMAGE_IMPORT_DESCRIPTOR*)(module_destination + import_directory_entry->VirtualAddress);
+
+	for (int import_module_index = 0; import_module_index  < ((import_directory_entry->Size / sizeof(IMAGE_IMPORT_DESCRIPTOR) - 1)); import_module_index++)
+	{
+		HMODULE import_module = modinfo->fnLoadLibrary((char*)((DWORD)module_destination + import_table[import_module_index].Name));
+			   
+		if (!import_module){
+			return 0;
+		}
+
+		// http://win32assembly.programminghorizon.com/pe-tut6.html
+		IMAGE_THUNK_DATA* originalThunkTable = (IMAGE_THUNK_DATA*)(module_destination + import_table[import_module_index].OriginalFirstThunk);
+		IMAGE_THUNK_DATA* thunkTable = (IMAGE_THUNK_DATA*)(module_destination + import_table[import_module_index].FirstThunk);
+		
+		for (int import_function_index = 0; thunkTable[import_function_index].u1.Function != 0 /*the table is zero terminated*/; import_function_index++) 
+		{
+			if (originalThunkTable && IMAGE_SNAP_BY_ORDINAL(originalThunkTable[import_function_index].u1.Ordinal))
+			{
+				//by ordinal
+				WORD ordinal = IMAGE_ORDINAL(originalThunkTable[import_function_index].u1.Ordinal);
+#ifdef WIN_X86
+				thunkTable[import_function_index].u1.Function = (DWORD)modinfo->fnGetProcAddress(import_module, MAKEINTRESOURCE(ordinal));
+#elif WIN_X64
+				thunkTable[import_function_index].u1.Function = (ULONGLONG)modinfo->fnGetProcAddress(import_module, MAKEINTRESOURCE(ordinal));
+#endif		
+			}
+			else
+			{
+				// by name
+				IMAGE_IMPORT_BY_NAME* thunkdata = (IMAGE_IMPORT_BY_NAME*)(module_destination + originalThunkTable[import_function_index].u1.AddressOfData);   				
+#ifdef WIN_X86
+				thunkTable[import_function_index].u1.Function = (DWORD)modinfo->fnGetProcAddress(import_module, thunkdata->Name);
+#elif WIN_X64
+				thunkTable[import_function_index].u1.Function = (ULONGLONG)modinfo->fnGetProcAddress(import_module, thunkdata->Name);
+#endif		   		   
+			}
+		}
+	}
 
 	//
 	// Set Section Memory flags
 	//
+	//						
+	for (int section_counter = 0; section_counter < new_headers->FileHeader.NumberOfSections; section_counter++)
+	{
+		IMAGE_SECTION_HEADER* ish = (IMAGE_SECTION_HEADER *)((DWORD)&new_headers->OptionalHeader + new_headers->FileHeader.SizeOfOptionalHeader + section_counter * sizeof(IMAGE_SECTION_HEADER));
+		DWORD memoryprotection = 0;
 
-	// Delete raw data
+		if (ish->Characteristics & IMAGE_SCN_MEM_DISCARDABLE)
+		{
+			modinfo->fnVirtualFree((LPVOID)ish->Misc.PhysicalAddress, ish->SizeOfRawData, MEM_DECOMMIT);
+			continue;
+		}  
 
-	return;
+		if (ish->Characteristics & IMAGE_SCN_MEM_EXECUTE) 
+		{
+			if (ish->Characteristics & IMAGE_SCN_MEM_READ) 
+			{
+				if (ish->Characteristics & IMAGE_SCN_MEM_WRITE)	
+				{
+					memoryprotection = PAGE_EXECUTE_READWRITE;
+				}											  
+				else 
+				{
+					memoryprotection = PAGE_EXECUTE_READ;
+				}
+			}
+			else 
+			{	  
+				if (ish->Characteristics & IMAGE_SCN_MEM_WRITE)	
+				{
+					memoryprotection = PAGE_EXECUTE_WRITECOPY;
+				}
+				else 
+				{									  
+					memoryprotection = PAGE_EXECUTE;
+				}
+			} 			
+		}
+		else 
+		{	   
+			if (ish->Characteristics & IMAGE_SCN_MEM_READ) 
+			{
+				if (ish->Characteristics & IMAGE_SCN_MEM_WRITE)	
+				{
+					memoryprotection = PAGE_READWRITE;
+				}
+				else 
+				{
+					memoryprotection = PAGE_READONLY;
+				}
+			}
+			else 
+			{
+				if (ish->Characteristics & IMAGE_SCN_MEM_WRITE)	
+				{
+					memoryprotection = PAGE_WRITECOPY;
+				}
+				else 
+				{
+					memoryprotection = PAGE_NOACCESS;
+				}
+			}
+		}
+
+		if (ish->Characteristics & IMAGE_SCN_MEM_NOT_CACHED)
+		{
+			memoryprotection |= PAGE_NOCACHE;
+		}
+
+		DWORD oldprot;
+		if (!modinfo->fnVirtualProtect((LPVOID)ish->Misc.PhysicalAddress, ish->SizeOfRawData, memoryprotection, &oldprot))
+		{
+			return 0;
+		}		   
+	}	
+	
+	//
+	// call Entrypoint
+	//
+	DWORD dllmainaddress = (DWORD)module_destination + new_headers->OptionalHeader.AddressOfEntryPoint;	
+	((DllMainFunction)dllmainaddress)((HINSTANCE)module_destination, DLL_PROCESS_ATTACH, NULL);
+
+	return module_destination;
 }	  	  	 
 					   
 void END_SHELLCODE(void) {}
+
+void Test()
+{
+	const char* dll = "C:\\Users\\Robin\\Documents\\Visual Studio 2017\\Projects\\TestDll\\Release\\TestDll.dll";
+	ShellcodeInformation* info = new ShellcodeInformation();
+
+	FILE* file;
+	fopen_s(&file, dll, "rb");
+	fseek(file, 0, SEEK_END);          // Jump to the end of the file
+	SIZE_T filelen = ftell(file);      // Get the current byte offset in the file
+	rewind(file);
+
+	byte* buffer = new byte[filelen + 1];
+	fread(buffer, filelen, 1, file); // Read in the entire file
+	fclose(file); // Close the file
+
+	info->raw_module_destination = buffer;
+	info->fnLoadLibrary = (LoadLibraryFunction)LoadLibraryA;
+	info->fnVirtualAlloc = (VirtualAllocFunction)VirtualAlloc;
+	info->fnVirtualFree = (VirtualFreeFunction)VirtualFree;
+	info->fnGetProcAddress = (GetProcAddressFunction)GetProcAddress;
+	info->fnVirtualProtect = (VirtualProtectFunction)VirtualProtect;
+
+	LoadLibShellcode(info);
+}
 
 
 int main(int argc, char *argv[])
 {
 
- // will not work in debug
+
 #ifdef _DEBUG
-	return 1337;
+	Test(); 
 #endif
 
 	FILE *output_file;
